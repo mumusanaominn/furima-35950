38ba120c99b8ec3a3c96551b718603c2a8c3fd74
mumusanaominn <aoi05152001@gmail.com>
2021-07-02 14:12:47 +0900

    ユーザー管理機能再提出

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -50,6 +50,7 @@ RSpec.describe User, type: :model do
       end
       it 'passwordが空では保存できないこと' do
         @user.password = ''
+        @user.password_confirmation = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Password can't be blank")
       end    
@@ -60,10 +61,10 @@ RSpec.describe User, type: :model do
         expect(another_user.errors.full_messages).to include('Email has already been taken')
       end
       it 'passwordとencrypted_password半角英数混合でないと保存できないこと' do
-        @user.password = ''
-        @user.encrypted_password = ''
+        @user.password = '000000'
+        @user.password_confirmation = '000000'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password can't be blank", "Password is invalid. Input half-width characters.")
+        expect(@user.errors.full_messages).to include("Password is invalid. Input half-width characters.")
       end
       it 'last_nameが空では保存できないこと' do
         @user.last_name = ''
@@ -87,9 +88,15 @@ RSpec.describe User, type: :model do
       end
       it 'passwordが5文字以下では保存できない' do
         @user.password = '00000'
-        @user.encrypted_password = '00000'
+        @user.password_confirmation = '00000'
         @user.valid?
         expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
+      end
+      it 'passwordが6文字以上であれば登録できること' do
+        @user.password = '000000'
+        @user.password_confirmation = '000000'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is invalid. Input half-width characters.")
       end
       it 'passwordが存在してもencrypted_passwordが空では保存できない' do
         @user.password = "000000aaaa"
@@ -101,6 +108,21 @@ RSpec.describe User, type: :model do
         @user.birthday = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Birthday can't be blank")
+      end
+      it 'passwordが数字だけでは保存できないこと' do
+        @user.password = '00000'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)", "Password is invalid. Input half-width characters.")
+      end
+      it 'passwordが英字だけでは保存できないこと' do
+        @user.password = 'aaa'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)", "Password is invalid. Input half-width characters.")
+      end
+      it 'passwordが全角文字が含まれている場合は保存できないこと' do
+        @user.password = '111あああ'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is invalid. Input half-width characters.")
       end
     end
   end
